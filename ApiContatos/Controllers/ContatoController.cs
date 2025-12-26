b5e01f8ced4387aa618c6890e23401c87f5e1b3e
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-12-26 08:19:22 -0300

    feat: getAll and getById functions added

---
+++ ApiContatos/Controllers/ContatoController.cs
@@ -0,0 +1,36 @@
+﻿using ApiContatos.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiContatos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ContatoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetTodos()
+        {
+            var contatos = await _context.Contatos.ToListAsync();
+            return Ok(contatos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPorId(int id) {
+            var contato = await _context.Contatos.FindAsync(id);
+            if (contato == null)
+            {
+                return NotFound();
+            }
+            return Ok(contato);
+        }
+    }
+}
